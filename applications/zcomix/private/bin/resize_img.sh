14b60722bc1126966915b5e130913f751b522e3f
Jim Karsten <iiijjjiii@gmail.com>
2014-08-29 19:37:14 -0400

    Mod 12491 - zcomix: Implement new resize script.

--- applications/zcomix/private/bin/resize_img.sh
+++ applications/zcomix/private/bin/resize_img.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 r1='GIF|JPEG|PNG'
-
 d1='cbz 1600 2560 625 1600
     web  750 1200 625 1600
     tbn  140  168 882 1133'
-
-#d1='cbz 1600 2560 625 1600'
-#d1='web  750 1200 625 1600'
-#d1='tbn  140  168 882 1133'
 
 _mi() { local e=$?; [[ -t 1 ]] && local g=$LIGHTGREEN coff=$COLOUROFF; printf "$g===: %s$coff\n" "$@"; return "$e"; }
 _mw() { [[ -t 1 ]] && local r=$RED coff=$COLOUROFF; printf "$r===: %s$coff\n" "$@"; return 1; } >&2
@@ -31,6 +26,20 @@ _check_files() {
     [[ $ext =~ $r1 ]] || _me "$i is not a GIF, PNG or JPEG image"
 }
 
+_colourmap() {
+    unset cj cm nc
+    if [[ ${f##*.} == jpg ]]; then
+        nc=$(identify -format '%k' "$f"[0])
+        (( $nc > 256 )) && return
+        cj="+dither -colors $nc"
+        nf=${nf/.jpg/.png}
+    fi
+
+    colours=$(convert "$f" $cj -unique-colors txt: | awk -v _=\" 'NR>1 {print "xc:"_$4_}')
+    eval convert -size 1x1 $colours +append colourmap.png
+    cm="-remap colourmap.png"
+}
+
 _resize() {
     while read -r fmt t1 t2 arl arh; do
         IFS=x read -r w h < <(identify -format '%P' "$f"[0])
@@ -41,28 +50,37 @@ _resize() {
 
         meta=$(identify -format '%[colorspace] %m' "$f"[0])
         [[ $meta != sRGB* ]]  && _mw "$f colorspace is not sRGB: $meta"
-        [[ $meta == CMYK* ]]  && cs=CMYK             ## convert CMYK to RBG
-        [[ $meta == *GIF  ]]  && nf=${nf%.*}.png     ## convert gif to png
-        [[ $meta == *PNG  ]]  && nf=${nf%.*}.png     ## set ext to png
-        [[ $meta == *JPEG  ]] && nf=${nf%.*}.jpg     ## set ext to jpg
+        [[ $meta == CMYK* ]]  && cs=CMYK            ## convert CMYK to RBG
+        [[ $meta == *GIF  ]]  && nf=${nf%.*}.png    ## convert gif to png
+        [[ $meta == *PNG  ]]  && nf=${nf%.*}.png    ## set ext to png
+        [[ $meta == *JPEG  ]] && nf=${nf%.*}.jpg    ## set ext to jpg
 
         ap=$(( $w*1000/$h ))
-        ## square: resize to large threshold (small for tbn)
-        if (( $ap > 940 && $ap < 1050 )); then
-            res=$t2
-            [[ $fmt == tbn ]] && res=$t1
-        elif ! (( $ap > 940 && $ap < 1050 )); then
-            ## if aspect ratio is between the limits && resize small threshold || resize large threshold
-            (( $ap > $arl && $ap < $arh )) && res=$t1 || res=$t2
+        if (( $ap > 940 && $ap < 1050 )); then              ## square: resize to large threshold (small for tbn)
+            [[ $fmt == cbz ]] && res=2080x                  ## (2560+1600)/2
+            [[ $fmt == web ]] && res=975x                   ## (1200+ 750)/2
+            [[ $fmt == tbn ]] && res=140x
+        elif [[ $fmt == web ]]; then
+            (( $w < $h )) && res=${t1}x || res=x$t2         ## if landscape, else portrait
         else
-            _me "wtf?: $f $fmt $s $l $filter $nf"
+            if (( $ap > $arl && $ap < $arh )); then         ## if aspect ratio is between the limits
+                (( $w < $h )) && res=${t1}x || res=x$t1     ## resize small threshold (t1)
+            else                                            ## else
+                (( $w < $h )) && res=x$t2 || res=${t2}x     ## resize large threshold (t2)
+            fi
         fi
 
-        [[ $res == $t1 ]] && { (( $w < $h )) && res=${res}x || res=x$res; }
-        [[ $res == $t2 ]] && { (( $w < $h )) && res=x$res || res=${res}x; }
-        filter="-gamma 1.333333333333333 -filter Catrom -resize $res -gamma .75 -colorspace sRGB +repage"
-#        echo convert "$f[0]" -quiet -colorspace "$cs" $filter "$nf"
-        convert "$f[0]" -quiet -colorspace "$cs" $filter "$nf"
+        filter=RobidouxSharp
+        _colourmap
+        convert \( "$f[0]" -quiet -define jpeg:preserve-settings -colorspace $cs \) \
+            \( -clone 0 -gamma 1.666666666666666 -filter $filter -distort Resize $res -gamma 0.6 \) \
+            \( -clone 0 -filter $filter -distort Resize $res \) -delete 0 \
+            \( -clone 1 -colorspace gray -auto-level \) -compose over -composite \
+            -set colorspace $cs -colorspace sRGB -depth 8 $cm +repage "$nf"
+        rm colourmap.png &>/dev/null
+
+        [[ ${nf##*.} == png ]] && pngcrush -q -ow "$nf"
+        [[ ${nf##*.} == jpg ]] && jpegtran -copy none -optimize -progressive "$nf" > foo.jpg && mv foo.jpg "$nf"
 
 #        IFS=x read -r nw nh < <(identify -format '%P' "$nf")
 #        printf "%s %04s %04s %s\n" $fmt $nw $nh $nf
@@ -73,27 +91,25 @@ _rename() {
     ext=$(identify -format '%m' "$i"[0] 2>/dev/null)
     [[ $ext == JPEG ]] && ext=jpg
     k=${j##*/}      ## strip off path
-    mv "$j" "ori-${k%.*}.${ext,,}"      ## ensure ext is correct
+    ori=ori-${k%.*}.${ext,,}
+    mv "$j" "$ori"  ## ensure ext is correct
+    [[ ${ori##*.} == png ]] && pngcrush -q -ow "$ori"
+    [[ ${ori##*.} == jpg ]] && jpegtran -copy none -optimize -progressive "$ori" > foo.jpg && mv foo.jpg "$ori"
+    return 0
 }
 
-command -v convert  &>/dev/null || { _me "convert not installed"; }
-command -v identify &>/dev/null || { _me "identify not installed"; }
-#command -v jpegtran &>/dev/null || { _me "jpegtran not installed"; }
-
+for i in convert identify jpegtran pngcrush; do command -v "$i" &>/dev/null || _me "$i not installed"; done
 (( $# == 0 )) && { _u; exit 1; }
 for i in $@; do _check_files; done
 for f in $@; do _resize; done
 for j in $@; do _rename; done
 
-## [ ] unit tests
-## [ ] add image optimization and stripping -- vips?
 
 #####################################
-
-#NARROW
-#11x16 1600x t:0.625  0.625 > && < 1.6 && resize width_x || resize height_x
-#16x11 x1600 t:1.6    0.625 > && < 1.6 && resize x_width || resize x_height
+# NARROW
+# 11x16 1600x t:0.625  0.625 > && < 1.6 && resize width_x || resize height_x
+# 16x11 x1600 t:1.6    0.625 > && < 1.6 && resize x_width || resize x_height
 #
-#SHORT
-#10x17 x2560
-#17x10 2560x
+# SHORT
+# 10x17 x2560
+# 17x10 2560x
