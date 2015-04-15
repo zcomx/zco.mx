5682ef7fb5e073bc65043edeed19818d7327cac4
Jim Karsten <iiijjjiii@gmail.com>
2015-04-15 15:08:10 -0400

    Mod 12814 - zcomix: Post notice of book release on twitter.

---
+++ applications/zcomx/private/tmp/twitter_poc.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+twitter_poc.py
+
+Script to POC test using python-twitter api.
+https://pypi.python.org/pypi/twitter
+"""
+import logging
+import sys
+import traceback
+from optparse import OptionParser
+from twitter import Twitter
+from twitter.oauth import OAuth
+
+VERSION = 'Version 0.1'
+
+LOG = logging.getLogger('cli')
+
+
+def man_page():
+    """Print manual page-like help"""
+    print """
+USAGE
+    twitter_poc.py
+
+OPTIONS
+    -h, --help
+        Print a brief help.
+
+    --man
+        Print man page-like help.
+
+    -v, --verbose
+        Print information messages to stdout.
+
+    --vv,
+        More verbose. Print debug messages to stdout.
+
+    """
+
+
+def main():
+    """Main processing."""
+
+    usage = '%prog [options]'
+    parser = OptionParser(usage=usage, version=VERSION)
+
+    parser.add_option(
+        '--man',
+        action='store_true', dest='man', default=False,
+        help='Display manual page-like help and exit.',
+    )
+    parser.add_option(
+        '-v', '--verbose',
+        action='store_true', dest='verbose', default=False,
+        help='Print messages to stdout.',
+    )
+    parser.add_option(
+        '--vv',
+        action='store_true', dest='vv', default=False,
+        help='More verbose.',
+    )
+
+    (options, unused_args) = parser.parse_args()
+
+    if options.man:
+        man_page()
+        quit(0)
+
+    if options.verbose or options.vv:
+        level = logging.DEBUG if options.vv else logging.INFO
+        unused_h = [
+            h.setLevel(level) for h in LOG.handlers
+            if h.__class__ == logging.StreamHandler
+        ]
+
+    LOG.info('Started.')
+
+    settings = current.app.local_settings
+
+    t = Twitter(
+        auth=OAuth(
+            settings.twitter_oauth_token,
+            settings.twitter_oauth_secret,
+            settings.twitter_consumer_key,
+            settings.twitter_consumer_secret
+        )
+    )
+    x = t.statuses.home_timeline()
+    # x = t.account.settings()
+    # x = t.statuses.user_timeline(screen_name="CharlesForsman", count=1)
+    # x = t.search.tweets(q="sittler", count=2)
+    # x = t.statuses.update(status='Testing')
+    # x = t.statuses.update(status='Development test http://zco.mx')
+    # x = t.statuses.destroy(id='586561746363621377')
+    print 'x: {var}'.format(var=x)
+    LOG.info('Done.')
+
+
+if __name__ == '__main__':
+    # W0703: *Catch "Exception"*
+    # pylint: disable=W0703
+    try:
+        main()
+    except Exception:
+        traceback.print_exc(file=sys.stderr)
+        exit(1)
