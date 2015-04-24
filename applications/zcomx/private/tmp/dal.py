6411628a731029f11f771f34bd303d7227b68d3e
Jim Karsten <iiijjjiii@gmail.com>
2015-04-24 17:50:55 -0400

    Mod 12832 - zcomix: Add pages column to the ongoing list view.

---
+++ applications/zcomx/private/tmp/dal.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+dal.py
+
+Script to test dal commands.
+"""
+import logging
+import sys
+import traceback
+from gluon import *
+from optparse import OptionParser
+
+VERSION = 'Version 0.1'
+
+LOG = logging.getLogger('cli')
+
+
+def man_page():
+    """Print manual page-like help"""
+    print """
+USAGE
+    dal.py
+
+OPTIONS
+    -h, --help
+        Print a brief help.
+
+    --man
+        Print man page-like help.
+
+    -v, --verbose
+        Print information messages to stdout.
+
+    --vv,
+        More verbose. Print debug messages to stdout.
+
+    """
+
+
+def main():
+    """Main processing."""
+
+    usage = '%prog [options]'
+    parser = OptionParser(usage=usage, version=VERSION)
+
+    parser.add_option(
+        '--man',
+        action='store_true', dest='man', default=False,
+        help='Display manual page-like help and exit.',
+    )
+    parser.add_option(
+        '-v', '--verbose',
+        action='store_true', dest='verbose', default=False,
+        help='Print messages to stdout.',
+    )
+    parser.add_option(
+        '--vv',
+        action='store_true', dest='vv', default=False,
+        help='More verbose.',
+    )
+
+    (options, unused_args) = parser.parse_args()
+
+    if options.man:
+        man_page()
+        quit(0)
+
+    if options.verbose or options.vv:
+        level = logging.DEBUG if options.vv else logging.INFO
+        unused_h = [
+            h.setLevel(level) for h in LOG.handlers
+            if h.__class__ == logging.StreamHandler
+        ]
+
+    LOG.info('Started.')
+    groupby = db.book_page.book_id
+    page_count = db.book.id.count()
+    max_on = db.book_page.created_on.max()
+    page2 = db.book_page.with_alias('page2')
+
+
+    rows = db(db.book).select(
+        db.book.id,
+        db.book.name,
+        page_count,
+        max_on,
+        left=[
+            db.creator.on(db.book.creator_id == db.creator.id),
+            db.auth_user.on(
+                db.creator.auth_user_id == db.auth_user.id
+            ),
+            db.book_page.on(db.book_page.book_id == db.book.id),
+            page2.on(
+                (page2.book_id == db.book.id) &
+                (page2.id != db.book_page.id) &
+                (page2.created_on < db.book_page.created_on)
+            ),
+        ],
+        groupby=groupby,
+    )
+    for r in rows:
+        print r.book.id, ' ', r.book.name, ' ', r[page_count], r[max_on]
+    print 'FIXME db._lastsql: {var}'.format(var=db._lastsql)
+
+    LOG.info('Done.')
+
+
+if __name__ == '__main__':
+    # W0703: *Catch "Exception"*
+    # pylint: disable=W0703
+    try:
+        main()
+    except Exception:
+        traceback.print_exc(file=sys.stderr)
+        exit(1)
