a568cffe11c538c2b1b77f72436b85af7c6188d5
Jim Karsten <iiijjjiii@gmail.com>
2015-06-24 11:20:59 -0400

    Mod 12918 - zcomix: Search autocompletion.

--- applications/zcomx/controllers/search.py
+++ applications/zcomx/controllers/search.py
@@ -1,93 +1,30 @@
 # -*- coding: utf-8 -*-
 """ Search controller."""
-from gluon.validators import urlify
 from applications.zcomx.modules.access import requires_login_if_configured
-from applications.zcomx.modules.books import \
-    formatted_name as formatted_book_name, \
-    url as book_url
-from applications.zcomx.modules.creators import \
-    formatted_name as formatted_creator_name, \
-    url as creator_url
+from applications.zcomx.modules.autocomplete import autocompleter_class
+from applications.zcomx.modules.books import url as book_url
+from applications.zcomx.modules.creators import url as creator_url
 from applications.zcomx.modules.search import classified
-from applications.zcomx.modules.zco import \
-    BOOK_STATUS_ACTIVE, \
-    Zco
+from applications.zcomx.modules.zco import Zco
 
 import logging
 LOG = logging.getLogger('app')
 
 
-def autocomplete_books():
-    """Return autocomplete results of books for search input.
+def autocomplete():
+    """Return autocomplete results for search input.
 
+    request.args(0): string, table, one of 'book' or 'creator'
     request.vars.q: string, the query keywords.
-        Returns all books if q is blank.
+        Returns all records if q is Falsy.
 
     """
     response.generic_patterns = ['json']
-
-    items = []
-
-    queries = []
-    queries.append((db.book.status == BOOK_STATUS_ACTIVE))
-    if request.vars.q:
-        kw = urlify(request.vars.q)
-        queries.append((db.book.name_for_search.contains(kw)))
-    query = reduce(lambda x, y: x & y, queries) if queries else None
-    rows = db(query).select(
-        db.book.id,
-        orderby=db.book.name,
-        distinct=True,
-    )
-
-    for r in rows:
-        items.append(
-            {
-                'id': r.id,
-                'table': 'book',
-                'value': formatted_book_name(
-                    db, r.id, include_publication_year=False)
-            }
-        )
-
-    return dict(results=items)
-
-
-def autocomplete_creators():
-    """Return autocomplete results of creators for search input.
-
-    request.vars.q: string, the query keywords.
-    """
-    response.generic_patterns = ['json']
-
-    items = []
-
-    queries = []
-    # Creators must have at least one book
-    queries.append((db.book.id != None))
-    if request.vars.q:
-        kw = urlify(request.vars.q)
-        queries.append((db.creator.name_for_search.contains(kw)))
-    query = reduce(lambda x, y: x & y, queries) if queries else None
-    rows = db(query).select(
-        db.creator.id,
-        left=[
-            db.book.on(db.book.creator_id == db.creator.id)
-        ],
-        orderby=db.creator.name_for_search,
-        distinct=True,
-    )
-
-    for r in rows:
-        items.append(
-            {
-                'id': r.id,
-                'table': 'creator',
-                'value': formatted_creator_name(r.id),
-            }
-        )
-
-    return dict(results=items)
+    completer_class = autocompleter_class(request.args(0))
+    if not completer_class:
+        return dict(results=[])
+    completer = completer_class(keyword=request.vars.q)
+    return dict(results=completer.search())
 
 
 def autocomplete_selected():
