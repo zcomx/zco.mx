4c2c284c62e58d298022cf147be668d9c4779ce5
Jim Karsten <iiijjjiii@gmail.com>
2020-12-02 10:24:47 -0500

    Mod 14049 - zcomx: Use pingo in optimize.sh.

--- applications/zcomx/private/bin/optimize_img.sh
+++ applications/zcomx/private/bin/optimize_img.sh
@@ -8,9 +8,54 @@ _u() { script=${0##*/}; cat << EOF
 usage: $script [file1, file2... fileN]
 
 This script optimizes JPG and PNG files.
-    --quick      Quick png optimization. Uses zopflipng -q
+    -Q      If JPG Quality is greater than 92, then lower it
 
 EOF
+}
+
+_check_files() {
+    identify "$i" &>/dev/null || _me "$i is not an image or the image is corrupt"
+    identify -regard-warnings -format "%g" "$i" &>/dev/null || _me "$i is corrupt"
+    ff=$(identify -format '%m' "$i"[0] 2>/dev/null); [[ $ff == JPEG ]] && ff=JPG     ## determine file format
+    r1='GIF|JPG|PNG'
+    [[ $ff =~ $r1 ]] || _me "$i is not a GIF, JPEG or PNG image"
+}
+
+_jq() {
+    jq=$(identify -format '%Q' "$i")
+    (( $jq > 92 )) && pingo -jpgquality=92 -jpgsample=2 -quiet "$i"
+}
+
+_optimize() {
+    t=tmp.$RANDOM
+
+    ext=${i##*.}
+    if [[ $ff != ${ext^^} ]]; then
+        _mi "File extension of $i was changed to: ${ff,,}"
+        mv "$i" "${i%.*}.${ff,,}"
+        i=${i%.*}.${ff,,}
+    fi
+
+    if [[ $ff == GIF ]]; then
+        convert "$i" "${i%.*}.png"
+        rm "$i"
+        ff=PNG
+        i="${i%.*}.png"
+    fi
+
+    if [[ $ff == PNG ]]; then
+        nice pingo -s9 -strip=3 "$i" >/dev/null || _me "pingo failed on file: $i"
+        [[ $t ]] && { defluff < "$i" > "$t" 2>/dev/null && mv "$t" "$i"; }
+    elif [[ $ff == JPG ]]; then
+        [[ $(identify -format '%[colorspace]' "$i") == CMYK ]] && { convert "$i" -colorspace sRGB "$t" && mv "$t" "$i"; }
+        [[ $Q ]] && _jq             ## If -Q, then check to see if jpg_quality should be lowered
+        jpegoptim -q -s "$i" || _me "jpegoptim failed on file: $i"
+    else
+        _me "File $i was not optimized"
+    fi
+
+    rm "$t" &>/dev/null
+    return 0
 }
 
 _options() {
@@ -20,7 +65,7 @@ _options() {
 
     while [[ $1 ]]; do
         case "$1" in
-       --quick) quick=1 ;;
+            -Q) Q=1             ;;
             -h) _u; exit 0      ;;
             --) shift; [[ $* ]] && args+=( "$@" ); break;;
             -*) _u; exit 0      ;;
@@ -32,38 +77,9 @@ _options() {
     (( ${#args[@]} == 0 )) && { _u; exit 1; }
 }
 
-
-_check_files() {
-    r1='JPEG|PNG'
-    identify "$i" &>/dev/null || _me "$i is not an image or image is corrupt"
-    identify -regard-warnings -format "%g" "$i" &>/dev/null || _me "$i is corrupt"
-    ff=$(identify -format '%m' "$i"[0] 2>/dev/null)     ## determine file format
-    [[ $ff =~ $r1 ]] || _me "$i is not a PNG or JPEG image"
-}
-
-_optimize() {
-    [[ $ff == JPEG ]] && ff=JPG
-    ext=${i##*.}
-    [[ $ff == ${ext^^} ]] || _me "File extension of $i should be $ff"
-
-    tmp=tmp.$RANDOM
-    if [[ $ff == PNG ]]; then
-        [[ $quick ]] && q='-q' || q=''
-        zopflipng $q "$i" "$tmp.png" >/dev/null || { break && _me "zopflipng failed on file: $i"; }
-        [[ $tmp.png ]] && defluff < "$tmp.png" > "$i" 2>/dev/null
-        rm "$tmp.png" &>/dev/null
-    elif [[ $ff == JPG ]]; then
-        jpegoptim -q -s "$i" "$tmp.jpg" || { break && _me "jpegoptim failed on file: $i"; }
-        [[ $tmp.jpg ]] && mv "$tmp.jpg" "$i" &>/dev/null
-        rm "$tmp.jpg" &>/dev/null
-    else
-        _me "File $i was not optimized"
-    fi
-    return 0
-}
-
 _options "$@"
 
-for i in defluff identify jpegoptim zopflipng; do command -v "$i" &>/dev/null || _me "$i not installed"; done
+for i in defluff convert identify jpegoptim pingo; do command -v "$i" &>/dev/null || _me "$i not installed"; done
 for i in "${args[@]}"; do _check_files; done
 for i in "${args[@]}"; do _optimize; done
+#[[ -t 1 ]] && printf '\a'       ## when done, ring the bell
