49594911c8b2cf6b265fd44ef517b678133b7463
Jim Karsten <iiijjjiii@gmail.com>
2020-12-03 15:20:56 -0500

    Mod 14041 - zcomx: Minor improvements and alterations.

--- applications/zcomx/private/sqlite/views.sql
+++ applications/zcomx/private/sqlite/views.sql
@@ -5,7 +5,9 @@ CREATE VIEW IF NOT EXISTS creator_grid_v AS
         sum(CASE WHEN b.release_date IS NOT NULL THEN 1 ELSE 0 END) as completed,
         sum(CASE WHEN b.release_date IS NULL THEN 1 ELSE 0 END) as ongoing,
         sum(b.downloads) as downloads,
-        sum(b.views) as views
+        sum(b.views) as views,
+        c.created_on as created_on,
+        c.updated_on as updated_on
     FROM creator c
     LEFT JOIN book b ON b.creator_id=c.id
     GROUP BY c.id
